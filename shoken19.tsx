71dcc715d959fc05e10d4cf6cf40b18ac038cca9
yair <yair99@gmail.com>
2020-12-02 23:48:31 +0200

    wip

---
+++ shoken19.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="shoken19" tilewidth="32" tileheight="32" tilecount="2072" columns="56">
+ <image source="shoken19.jpg" width="1816" height="1196"/>
+</tileset>
